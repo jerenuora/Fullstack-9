b469d555f98570956095b8a1613aeda45d66e80b
Jere Nuora <jere.nuora@gmail.com>
2022-03-26 18:20:55 +0200

    added forgotten semicolons

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,22 +1,22 @@
 const calculateBmi = (height: number, weight: number): string => {
-    const BMI = weight/((height/100)**2) 
+    const BMI = weight/((height/100)**2);
     if (BMI < 16.0) {
-        return "Underweight (Severe thinness)"
+        return "Underweight (Severe thinness)";
     } else if (BMI < 17.0) {
-        return "Underweight (Moderate thinness) "
+        return "Underweight (Moderate thinness) ";
     } else if (BMI < 18.5) {
-        return "Underweight (Mild thinness) "
+        return "Underweight (Mild thinness) ";
     } else if (BMI < 25.0) {
-        return "Normal range "
+        return "Normal range ";
     } else if (BMI < 30.0) {
-        return "Overweight (Pre-obese) "
+        return "Overweight (Pre-obese) ";
     } else if (BMI < 35.0) {
-        return "Obese (Class I) "
+        return "Obese (Class I) ";
     } else if (BMI < 40.0) {
-        return "Obese (Class II) "
+        return "Obese (Class II) ";
     } else {
-        return "Obese (Class III) "
-    }
-}
+        return "Obese (Class III) ";
+    };
+};
 
-console.log(calculateBmi(180, 74))
+console.log(calculateBmi(180, 74));
