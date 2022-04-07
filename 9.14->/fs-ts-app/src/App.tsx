3541e26dccc9848955d4d3c39f04af7585ae0d1f
Jere Nuora <jere.nuora@gmail.com>
2022-04-07 14:22:40 +0300

    description interface added

--- 9.14->/fs-ts-app/src/App.tsx
+++ 9.14->/fs-ts-app/src/App.tsx
@@ -1,7 +1,32 @@
+// new types
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+interface CourseDescriptionPart extends CoursePartBase {
+  description: string;
+}
+interface CourseNormalPart extends CourseDescriptionPart {
+  type: "normal";
+}
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CourseDescriptionPart {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
+
 const Header = ({ name }: { name: string }) => {
   return  <h1>{name}</h1>
 
-}
+};
+
 interface Parts {
   courseParts: {
     name: string;
@@ -18,8 +43,8 @@ const Content = ( { courseParts }: Parts ) => {
       {part.name} {part.exerciseCount}
       </p>
     </div>)}
-  </>)
-}
+  </>);
+};
 const Total = ( { courseParts }: Parts ) => {
   return (
     <div>
@@ -30,24 +55,37 @@ const Total = ( { courseParts }: Parts ) => {
 
     </div>
   )
-}
+};
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the harded course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
     }
   ];
-
+  
   return (
     <div>
       <Header name={courseName}/>
